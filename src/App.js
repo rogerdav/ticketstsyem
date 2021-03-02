c50c7ece5567503fb08e228616c9288266703564
Roger Davenport <roger.neil.davenport@gmail.com>
2021-03-02 14:24:11 -0800

    starting point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <form autoComplete="off" id="ticketForm">
+          <TextField id="name" varient="outlined" label="Name" />
+          <TextField id="location" varient="outlined" label="Location" />
+          <TextField id="description" varient="outlined" label="Description" />
+
+        
+        </form>
+      </Container>
+      
     </div>
   );
 }
